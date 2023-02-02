3a83f218b72cb06ce7c54cb4c719864444295138
Mark <mark.gowen78@gmail.com>
2023-02-02 15:46:50 -0800

    styled NavBar

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -2,16 +2,16 @@ import Link from 'next/link'
 
 export default function NavBar() {
     return (
-        <div className='grid grid-cols-4 text-sm'>
-            <p className='col-span-3 mt-5 px-10 place-content-start text-aqua'>
+        <div className='grid grid-cols-4'>
+            <p className='col-span-3 mt-5 text-base px-10 place-content-start text-aqua'>
                 Mark Gowen
             </p>
-            <ul className="flex place-content-end mt-5 px-10 text-aqua">
-                <li className="px-5"><Link href="/">About</Link></li>
-                <li className="px-5"><Link href="/">Work</Link></li>
-                <li className="px-5"><Link href="/">Projects</Link></li>
-                <li className="px-5"><Link href="/">Contact</Link></li>
-                <li className="px-5"><Link href="/">Admin</Link></li>
+            <ul className="flex items-center place-content-end mt-5 px-20  text-sm text-aqua">
+                <li className="px-5 hover:text-silver"><Link href="/"><em className='text-salmon mr-2'>//</em>About</Link></li>
+                <li className="px-5 hover:text-silver"><Link href="/"><em className='text-salmon mr-2'>//</em>Work</Link></li>
+                <li className="px-5 hover:text-silver"><Link href="/"><em className='text-salmon mr-2'>//</em>Projects</Link></li>
+                <li className="px-5 hover:text-silver"><Link href="/"><em className='text-salmon mr-2'>//</em>Contact</Link></li>
+                <button className="ml-5 px-5 py-1 border border-aqua rounded-none hover:text-silver hover:border-silver">Admin</button>
             </ul>
         </div>
     )
