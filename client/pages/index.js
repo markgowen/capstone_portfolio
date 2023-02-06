663dbe18706c2b137e36d80f53cf95fa5a65008c
Mark <mark.gowen78@gmail.com>
2023-02-06 14:04:48 -0800

    fixed merge conflicts

--- client/pages/index.js
+++ client/pages/index.js
@@ -8,18 +8,14 @@ export default function Home() {
     <>
       <Head>
         <title>Mark Gowen</title>
-<<<<<<< HEAD
         <meta name="description" content="Software Engineering Portfolio" />
-=======
-        <meta name="description" content="Software Engineering Portfolio" />
->>>>>>> ca7be46 (updated index.js to fix merge conflict)
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head >
-    <main>
-      <div>
-      </div>
-    </main>
+      <main>
+        <div>
+        </div>
+      </main>
     </>
   )
 }
