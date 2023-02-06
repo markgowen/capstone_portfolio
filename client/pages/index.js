5988630948bb112881db70e19879a30c51db51e3
Mark <mark.gowen78@gmail.com>
2023-02-06 13:49:54 -0800

    Configured Employer and Project Controllers

--- client/pages/index.js
+++ client/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     <>
       <Head>
         <title>Mark Gowen</title>
-        <meta name="description" content="Software Engineer Portfolio" />
+        <meta name="description" content="Software Engineering Portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
