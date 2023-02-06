4f6d916dcbe6e70d2d4eab18730602d6454ccdaf
Mark <mark.gowen78@gmail.com>
2023-02-06 13:49:54 -0800

    Configured Employer and Project Controllers

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -1,2 +1,30 @@
 class EmployersController < ApplicationController
+  def index
+    render json Employer.all
+  end
+
+  def show
+    render json: @employer
+  end
+
+  def create
+    employer = Employer.create!(employer_params)
+    render json: employer, status: :created
+  end
+
+  def update
+    @employer.update!(employer_params)
+    render json: @employer, status: :accepted
+  end
+
+  def destroy
+    @employer.destroy
+    head :no_content
+  end
+
+  private
+
+  def employer_params
+    params.permit(:name, :location, :role, :time_employed, :desc_1, :desc_2, :desc_3, :desc_4, :desc_5)
+  end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,30 @@
 class ProjectsController < ApplicationController
+  def index
+    render json Project.all
+  end
+
+  def show
+    render json: @project
+  end
+
+  def create
+    project = Project.create!(project_params)
+    render json: project, status: :created
+  end
+
+  def update
+    @project.update!(project_params)
+    render json: @project, status: :accepted
+  end
+
+  def destroy
+    @project.destroy
+    head :no_content
+  end
+
+  private
+
+  def project_params
+    params.permit(:name, :description, :framework_1, :framework_2, :framework_3, :framework_4, :framework_5, :link)
+  end
 end
