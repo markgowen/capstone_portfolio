dc3b05cefce3025ea2f545829e6b5cbb77882ad0
Mark <mark.gowen78@gmail.com>
2023-02-15 09:50:29 -0800

    configured navbar to only display admin button on /

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,10 +1,14 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import { useEffect, useState, useContext } from 'react'
+import { useRouter } from 'next/router'
 import UserContext from '@/contexts/userContext';
 import Link from 'next/link'
 
 export default function NavBar() {
     const [user, setUser] = useContext(UserContext);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const router = useRouter();
 
     useEffect(() => {
         fetch("/api/me").then((res) => {
@@ -17,6 +21,15 @@ export default function NavBar() {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    const loggedIn = user ? true : false;
+
+    let button;
+    if (loggedIn) {
+        button = <button className="ml-5 px-5 py-1 border border-aqua rounded-none hover:text-silver hover:border-silver"><Link href="/dashboard">Admin</Link></button>
+    } else {
+        button = <button className="ml-5 px-5 py-1 border border-aqua rounded-none hover:text-silver hover:border-silver"><Link href="/login">Admin</Link></button>
+    };
+
     return (
         <div className='grid grid-cols-4'>
             <p className='z-10 col-span-3 mt-5 text-base px-10 place-content-start text-aqua'><Link href="/">Mark Gowen</Link></p>
@@ -25,7 +38,8 @@ export default function NavBar() {
                 <li className="px-5 hover:text-silver"><Link href="/"><em className='text-salmon mr-2'>//</em>Work</Link></li>
                 <li className="px-5 hover:text-silver"><Link href="/"><em className='text-salmon mr-2'>//</em>Projects</Link></li>
                 <li className="px-5 hover:text-silver"><Link href="/contact"><em className='text-salmon mr-2'>//</em>Contact</Link></li>
-                {!user ? <button className="ml-5 px-5 py-1 border border-aqua rounded-none hover:text-silver hover:border-silver"><Link href="/login">Admin</Link> </button> : null}
+                {/* {router.asPath != "/dashboard" ? <button className="ml-5 px-5 py-1 border border-aqua rounded-none hover:text-silver hover:border-silver"><Link href="/login">Admin</Link></button> : null} */}
+                {router.asPath != "/dashboard" ? button : null}
             </ul>
         </div>
     )
