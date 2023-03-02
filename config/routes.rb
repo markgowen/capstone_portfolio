ee84fc8c74c9ef15fd552f38663d01fe7fcb4ec3
Mark <mark.gowen78@gmail.com>
2023-03-02 12:51:38 -0600

    updated database_url

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 
-  get "/",
+  get "*path",
       to: "fallback#index",
       constraints: ->(req) { !req.xhr? && req.format.html? }
 end
